d3f6911f67301da508976dda855427bbbba682fe
liorbondo <liorbondo@gmail.com>
2021-02-18 10:33:55 +0200

    tester

--- ActionManager/actionTester.py
+++ ActionManager/actionTester.py
@@ -7,13 +7,25 @@ import json
 #'lon': 34.978807518673285
 
 r = redis.StrictRedis(host='docksegenredis.redis.cache.windows.net',
-        port=6379, db=0, password='', ssl=False)
+        port=6379, db=0, password='=', ssl=False)
 
 for i in range(100):
-        eid = 'entity:' + str(i)
-        r.hset(eid, 'lat', 32.226402272851416)
-        r.hset(eid, 'lat', 34.9905663231064)
+        
+        # init entities in different start point locations
+        delta = 0.001 * i
+        lat = 32.226402272851416 + delta
+        lon = 34.9905663231064 + delta
 
+        eid = 'entity:' + str(i)
+        name = 'marina' + str(i)
+        r.hset(eid, mapping={"name": name, "lat": lat, "lon": lon})
+
+        #r.hset(eid, 'lat', lat)
+        #r.hset(eid, 'lon', lon)
+        
+        r.sadd("entities", eid)
+
+         # request entities in the same end point locations
         data = {
                 'id': i,
                 'lat': 32.15317153318547,
